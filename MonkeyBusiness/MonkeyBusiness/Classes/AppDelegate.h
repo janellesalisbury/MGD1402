5d2acc1ebbd35444f62a5a1943c4e36c779805ca
Janelle Salisbury <janellesalisbury@fullsail.edu>
2014-02-04 16:40:54 -0500

    Dowloading Cocos2d and testing out product

---
+++ MonkeyBusiness/MonkeyBusiness/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  MonkeyBusiness
+//
+//  Created by Janelle Salisbury on 2/4/14.
+//  Copyright Janelle Salisbury 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
