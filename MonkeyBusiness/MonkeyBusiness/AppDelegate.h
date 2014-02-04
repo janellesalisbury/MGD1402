1fe691fe0fca48a466f9a527ac82db2655319e62
Janelle Salisbury <janellesalisbury@fullsail.edu>
2014-02-04 15:55:15 -0500

    MGD Files

---
+++ MonkeyBusiness/MonkeyBusiness/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MonkeyBusiness
+//
+//  Created by Janelle Salisbury on 2/4/14.
+//  Copyright (c) 2014 Janelle Salisbury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
