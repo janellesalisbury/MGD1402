f57434dbfb1bd5691763496c40c58e3d45824ddf
Janelle Salisbury <janellesalisbury@fullsail.edu>
2014-02-06 17:15:02 -0500

    Lets try this again!

---
+++ project_one/project_one/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  project_one
+//
+//  Created by Janelle Salisbury on 2/6/14.
+//  Copyright Janelle Salisbury 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
