75cf095a5471be0497e1744320a74bc8e0699258
Janelle Salisbury <janellesalisbury@fullsail.edu>
2014-02-06 18:16:58 -0500

    new

---
+++ MGD_ProjectOne/MGD_ProjectOne/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  MGD_ProjectOne
+//
+//  Created by Janelle Salisbury on 2/6/14.
+//  Copyright Janelle Salisbury 2014. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
