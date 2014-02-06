4a0f2cfaa7692daa468efe2994c24d43a1024724
Janelle Salisbury <janellesalisbury@fullsail.edu>
2014-02-06 16:44:12 -0500

    alright lets get this moving

---
+++ project_one/project_one/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  project_one
+//
+//  Created by Janelle Salisbury on 2/6/14.
+//  Copyright Janelle Salisbury 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
