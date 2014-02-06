3b65f00dd1de14167fcf508c76936e2572c2960e
Janelle Salisbury <janellesalisbury@fullsail.edu>
2014-02-06 17:32:27 -0500

    3 sprites being displayed on screen

--- project_one/project_one/HelloWorldLayer.m
+++ project_one/project_one/HelloWorldLayer.m
@@ -7,15 +7,17 @@
 //
 
 
-// Import the interfaces
-#import "HelloWorldLayer.h"
 
-// Needed to obtain the Navigation Controller
+#import "HelloWorldLayer.h"
 #import "AppDelegate.h"
+
+CCSprite *turtle;
+CCSprite *pig;
+CCSprite *snake;
 
 #pragma mark - HelloWorldLayer
 
-// HelloWorldLayer implementation
+
 @implementation HelloWorldLayer
 
 // Helper class method that creates a Scene with the HelloWorldLayer as the only child.
@@ -34,75 +36,31 @@
 	return scene;
 }
 
-// on "init" you need to initialize your instance
+
 -(id) init
 {
-	// always call "super" init
-	// Apple recommends to re-assign "self" with the "super's" return value
-	if( (self=[super init]) ) {
-		
-		// create and initialize a Label
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"Marker Felt" fontSize:64];
+    
+    if((self = [super init])){
+        //add turtle sprite
+        turtle = [CCSprite spriteWithFile:@"turtle_64x72.png"];
+        turtle.position = ccp(175,120);
+        [self addChild:turtle];
+        
+        //add pig sprite
+        pig = [CCSprite spriteWithFile:@"pig_64x72.png"];
+        pig.position = ccp(275,150);
+        [self addChild:pig];
+        
+        //add snake sprite
+        snake = [CCSprite spriteWithFile:@"snake.png"];
+        snake.position = ccp(375,110);
+        [self addChild:snake];
+        
+        
+    }
+    
+    
 
-		// ask director for the window size
-		CGSize size = [[CCDirector sharedDirector] winSize];
-	
-		// position the label on the center of the screen
-		label.position =  ccp( size.width /2 , size.height/2 );
-		
-		// add the label as a child to this Layer
-		[self addChild: label];
-		
-		
-		
-		//
-		// Leaderboards and Achievements
-		//
-		
-		// Default font size will be 28 points.
-		[CCMenuItemFont setFontSize:28];
-		
-		// to avoid a retain-cycle with the menuitem and blocks
-		__block id copy_self = self;
-		
-		// Achievement Menu Item using blocks
-		CCMenuItem *itemAchievement = [CCMenuItemFont itemWithString:@"Achievements" block:^(id sender) {
-			
-			
-			GKAchievementViewController *achivementViewController = [[GKAchievementViewController alloc] init];
-			achivementViewController.achievementDelegate = copy_self;
-			
-			AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
-			
-			[[app navController] presentModalViewController:achivementViewController animated:YES];
-			
-			[achivementViewController release];
-		}];
-		
-		// Leaderboard Menu Item using blocks
-		CCMenuItem *itemLeaderboard = [CCMenuItemFont itemWithString:@"Leaderboard" block:^(id sender) {
-			
-			
-			GKLeaderboardViewController *leaderboardViewController = [[GKLeaderboardViewController alloc] init];
-			leaderboardViewController.leaderboardDelegate = copy_self;
-			
-			AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
-			
-			[[app navController] presentModalViewController:leaderboardViewController animated:YES];
-			
-			[leaderboardViewController release];
-		}];
-
-		
-		CCMenu *menu = [CCMenu menuWithItems:itemAchievement, itemLeaderboard, nil];
-		
-		[menu alignItemsHorizontallyWithPadding:20];
-		[menu setPosition:ccp( size.width/2, size.height/2 - 50)];
-		
-		// Add the menu to the layer
-		[self addChild:menu];
-
-	}
 	return self;
 }
 
